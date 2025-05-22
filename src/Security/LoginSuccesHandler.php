f22d7b11cf603be4a9983a7276ecfc880673c5db
Taniahrinialy <Taniahnyaian@gmail.com>
2025-05-22 14:55:47 +0300

    login

---
+++ src/Security/LoginSuccesHandler.php
@@ -0,0 +1,34 @@
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    private RouterInterface $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): RedirectResponse
+    {
+        $user = $token->getUser();
+        $roles = $user->getRoles();
+
+        if (in_array('ROLE_ADMIN', $roles)) {
+            return new RedirectResponse($this->router->generate('app_categories_index'));
+        }
+
+        if (in_array('ROLE_USER', $roles)) {
+            return new RedirectResponse($this->router->generate('app_articles_index'));
+        }
+
+        // Fallback
+        return new RedirectResponse($this->router->generate('app_auth_login'));
+    }
+}
